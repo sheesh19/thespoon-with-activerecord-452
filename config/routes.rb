8553ff0b28981355f2c7f3906b56cd27e7e9769e
Sheila Leveille <sbleveille@gmail.com>
2020-09-01 11:05:19 +1000

    ARCH: 7 CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants
+  
+  # only index & show routes
+  # resources :restaurants, only: [:index, :show]
+
+  # verb prefix/url, to: 'controller#action', as: :alias
+  # # READ ALL
+  # get 'restaurants', to: 'restaurants#index'
+
+  # # CREATE
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # READ ONE
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  
+  # # UPDATE
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # # DELETE ONE
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
 end
